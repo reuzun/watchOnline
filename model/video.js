030a76af0ed8a3d1b871132f1e0d75d9ccadb2f7
reuzun <moruka12@yandex.com>
2021-10-11 14:51:14 +0300

    room frontend added. room get request handled. Code format. OOP init.

---
+++ model/video.js
@@ -0,0 +1,8 @@
+export default class Video {
+    constructor(videoId, time, status) {
+        this.videoId = videoId;
+        this.time = time;
+        this.status = status;
+    }
+
+};
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const express = require('express');
+import express, { json, urlencoded } from 'express';
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
+import { createServer } from 'http';
+const server = createServer(app);
+import { Server } from "socket.io";
 const io = new Server(server);
 
 // Setting env variables.
-const dotenv = require('dotenv');
-const { Socket } = require('dgram');
-dotenv.config();
+import { config } from 'dotenv';
+import { Socket } from 'dgram';
+config();
 const API_SERVER_PORT = process.env.PORT || 3000;
 const URL = process.env.URL || `http://localhost:${API_SERVER_PORT}`
 
-availableRooms = [];
+
 
 app.use(express.static('public'));
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+app.use("/model", express.static('model'));
+app.use(json());       // to support JSON-encoded bodies
+app.use(urlencoded({ extended: true })); // to support URL-encoded bodies
 
 
 app.set('view engine', 'ejs');
@@ -29,97 +30,88 @@ app.get('/', (req, res) => {
 app.post('/room', (req, res) => {
   res.redirect(308, `/room/${req.body.room}/video/${CvideoId}`)
 });
+/*
+import Video from "./model/video.js";
+let firstVideo = new Video('9VksF2IXlQw', 0, 1);
+console.log(firstVideo)*/
+var CvideoId = firstVideo.videoId;
 
-var globalStatus = 1;
-//var globalTime = 0;
-var CvideoId = "9VksF2IXlQw";
+let availableRooms = [];
+let roomDatas = {}; // Uses key as roomId and an object value as datas including videoId
 
-roomDatas = {}; // Uses key as roomId and an object value as datas including videoId
+app.use('/room/:roomId/video/:videoId', (req, res) => {
 
-app.post('/room/:roomId/video/:videoId', (req, res) => {
-  
-  /*res.render('roomAlternative.ejs', {
-    roomId: `${req.body.room}`,
-    videoId: `${CvideoId}`,
-  });*/
 
-  /*io.of(`/room/${req.body.room}`).on('connection', async (socket)=>{
-    // let roomId = socket.handshake.auth.roomId;
-    socket.emit('message', `message from room ${req.body.room}`)
-  })*/
-
-  res.render('roomAlternative.ejs', {
-    roomId: `${req.body.room}`,
-    videoId: `${roomDatas[req.body.room] ? roomDatas[req.body.room].vid : CvideoId}`,
+  res.render('room.ejs', {
+    roomId: `${req.params.roomId}`,
+    videoId: `${roomDatas[req.params.roomId] ? roomDatas[req.params.roomId].vid : CvideoId}`,
     PORT: API_SERVER_PORT,
     URL: URL,
   });
   // Creating new Room
-  if (!availableRooms.includes(req.body.room)) {
+  if (!availableRooms.includes(req.params.roomId)) {
 
-    io.of(`/room/${req.body.room}`).on('connection', (socket) => {
-     
+    io.of(`/room/${req.params.roomId}`).on('connection', (socket) => {
+
       socket.on('currentTime', (time, vid, roomId) => {
-          roomDatas[roomId] = {vid:vid, time:time, status:1} // An if may required
-          console.log(roomDatas[roomId].time)
-        });
+        roomDatas[roomId] = { vid: vid, time: time, status: 1 } // An if may required
+      });
 
       socket.on('videoChange', (vid, roomId) => {
-        roomDatas[roomId] = {vid:vid, time:0, status:1}
+        roomDatas[roomId] = { vid: vid, time: 0, status: 1 }
         socket.broadcast.emit('clientVideoChange', vid)
       });
 
 
 
-      socket.on('statusChanged', (status)=>{
+      socket.on('statusChanged', (status) => {
         socket.broadcast.emit('handleStatus', status);
       });
 
-      socket.on('clientSeek', (time)=>{
+      socket.on('clientSeek', (time) => {
         socket.broadcast.emit('timeChanged', (time));
       });
 
 
-      socket.on('getTime', (rid, uid)=>{
-        console.log('time'); console.log(roomDatas[rid]?roomDatas[rid].time:'undefined!' )
+      socket.on('getTime', (rid, uid) => {
         socket.emit('comingTime', uid, roomDatas[rid].time)
       })
 
 
     });
 
-    availableRooms.push(req.body.room)
-  } else { 
-    io.of(`/room/${req.body.room}`).on('connection', (socket) => {
+    availableRooms.push(req.params.roomId)
+  } else {
+    io.of(`/room/${req.params.roomId}`).on('connection', (socket) => {
 
-      socket.on('init', (roomId)=>{
-        socket.emit('seek',  roomDatas[roomId].time,  roomDatas[roomId].status,  roomDatas[roomId].vid);
+      socket.on('init', (roomId) => {
+        socket.emit('seek', roomDatas[roomId].time, roomDatas[roomId].status, roomDatas[roomId].vid);
       });
-      
+
       socket.on('currentTime', (time, vid, roomId) => {
-        roomDatas[roomId] = {vid:vid, time:time, status:1} // An if may required
+        roomDatas[roomId] = { vid: vid, time: time, status: 1 } // An if may required
       })
 
       socket.on('videoChange', (vid, roomId) => {
-        roomDatas[roomId] = {vid:vid, time:0, status:1}
+        roomDatas[roomId] = { vid: vid, time: 0, status: 1 }
         socket.broadcast.emit('clientVideoChange', vid)
       })
 
-      socket.on('statusChanged', (status)=>{
+      socket.on('statusChanged', (status) => {
         socket.broadcast.emit('handleStatus', status);
       })
 
-      socket.on('clientSeek', (time)=>{
+      socket.on('clientSeek', (time) => {
         socket.broadcast.emit('timeChanged', (time));
       });
 
-      socket.on('getTime', (rid, uid)=>{
+      socket.on('getTime', (rid, uid) => {
         socket.emit('comingTime', uid, roomDatas[rid].time)
       })
 
     });
 
-    
+
 
   }
 
